bfd9f499b32716fb9057e8835c45a9ff590136c0
Rozanna Royter <rozzy.royter@gmail.com>
2020-07-29 17:26:08 +0300

    added try catch to pymysql

--- db_utils.py
+++ db_utils.py
@@ -76,105 +76,109 @@ def save_user_info(conn, users_dict):
         trips = user_info[config.NAMES_DICT["TRIP_LIST"]]
         trips_list = []
 
-        with conn.cursor() as cur:
-            if username not in existing_users_dict.keys():
+        try:
+            with conn.cursor() as cur:
+                if username not in existing_users_dict.keys():
 
-                cur.execute(config.INSERT_SCRAPED_INFO,
-                            (user_info['user_id'], username, user_info['follower-count'], user_info['following-count'],
-                             user_info['trips-count'], user_info['distance-traveled'], user_info['countries-count'],
-                             user_info['cities-count'], user_info['twitter'], user_info['instagram'], user_info['bio'])
-                            )
-                conn.commit()
-                current_user_id = cur.lastrowid
-
-                twitter_details = user_info['twitter_details']
-                if twitter_details:
-                    cur.execute(config.INSERT_TWITTER_INFO,
-                                (current_user_id, twitter_details['id_str'], twitter_details['screen_name'],
-                                    twitter_details['location'], twitter_details['description'],
-                                    twitter_details['followers_count'], twitter_details['friends_count'],
-                                    twitter_details['favourites_count'], twitter_details['verified'],
-                                    twitter_details['statuses_count']
-                                 )
+                    cur.execute(config.INSERT_SCRAPED_INFO,
+                                (user_info['user_id'], username, user_info['follower-count'], user_info['following-count'],
+                                 user_info['trips-count'], user_info['distance-traveled'], user_info['countries-count'],
+                                 user_info['cities-count'], user_info['twitter'], user_info['instagram'], user_info['bio'])
                                 )
                     conn.commit()
+                    current_user_id = cur.lastrowid
 
-            else:
-                cur.execute(config.UPDATE_USER,
-                            (
-                                user_info['follower-count'], user_info['following-count'], user_info['trips-count'],
-                                user_info['distance-traveled'], user_info['countries-count'], user_info['cities-count'],
-                                user_info['twitter'], user_info['instagram'], user_info['bio'],
-                                existing_users_dict[username]
-                            )
-                            )
-                conn.commit()
-                current_user_id = existing_users_dict[username]
-
-                twitter_details = user_info['twitter_details']
-
-                if twitter_details:
-                    query_tw = config.SELECT_TWITTER_USER.format(current_user_id)
-                    cur.execute(query_tw)
-                    tu_id = cur.fetchall()
-
-                    if not tu_id:
+                    twitter_details = user_info['twitter_details']
+                    if twitter_details:
                         cur.execute(config.INSERT_TWITTER_INFO,
                                     (current_user_id, twitter_details['id_str'], twitter_details['screen_name'],
-                                     twitter_details['location'], twitter_details['description'],
-                                     twitter_details['followers_count'], twitter_details['friends_count'],
-                                     twitter_details['favourites_count'], twitter_details['verified'],
-                                     twitter_details['statuses_count']
+                                        twitter_details['location'], twitter_details['description'],
+                                        twitter_details['followers_count'], twitter_details['friends_count'],
+                                        twitter_details['favourites_count'], twitter_details['verified'],
+                                        twitter_details['statuses_count']
                                      )
                                     )
                         conn.commit()
 
-                    else:
-                        cur.execute(config.UPDATE_TWITTER_INFO,
-                                    (twitter_details['id_str'], twitter_details['screen_name'],
-                                     twitter_details['location'], twitter_details['description'],
-                                     twitter_details['followers_count'], twitter_details['friends_count'],
-                                     twitter_details['favourites_count'], twitter_details['verified'],
-                                     twitter_details['statuses_count'], current_user_id
-                                     )
-                                    )
-                        conn.commit()
+                else:
+                    cur.execute(config.UPDATE_USER,
+                                (
+                                    user_info['follower-count'], user_info['following-count'], user_info['trips-count'],
+                                    user_info['distance-traveled'], user_info['countries-count'], user_info['cities-count'],
+                                    user_info['twitter'], user_info['instagram'], user_info['bio'],
+                                    existing_users_dict[username]
+                                )
+                                )
+                    conn.commit()
+                    current_user_id = existing_users_dict[username]
 
-            trips_query = config.SELECT_TRIPS.format(current_user_id)
-            cur.execute(trips_query)
-            existing_trips = cur.fetchall()
-            existing_trips_ext_ids = [t['external_id'] for t in existing_trips]
-            for trip_id in trips:
-                if trip_id not in existing_trips_ext_ids:
+                    twitter_details = user_info['twitter_details']
 
-                    query = config.SELECT_CITY_COUNTRY.format(trips[trip_id]['name'], trips[trip_id]['country'])
-                    cur.execute(query)
-                    cc_id = cur.fetchall()
+                    if twitter_details:
+                        query_tw = config.SELECT_TWITTER_USER.format(current_user_id)
+                        cur.execute(query_tw)
+                        tu_id = cur.fetchall()
 
-                    if not cc_id:
-
-                        query2 = config.SELECT_COUNTRY
-                        cur.execute(query2, trips[trip_id]['country'])
-                        country_id = cur.fetchall()
-
-                        if not country_id:
-                            cur.execute(config.INSERT_COUNTRY, (trips[trip_id]['country']))
+                        if not tu_id:
+                            cur.execute(config.INSERT_TWITTER_INFO,
+                                        (current_user_id, twitter_details['id_str'], twitter_details['screen_name'],
+                                         twitter_details['location'], twitter_details['description'],
+                                         twitter_details['followers_count'], twitter_details['friends_count'],
+                                         twitter_details['favourites_count'], twitter_details['verified'],
+                                         twitter_details['statuses_count']
+                                         )
+                                        )
                             conn.commit()
-                            country_id = cur.lastrowid
+
                         else:
-                            country_id = country_id[0]['id']
+                            cur.execute(config.UPDATE_TWITTER_INFO,
+                                        (twitter_details['id_str'], twitter_details['screen_name'],
+                                         twitter_details['location'], twitter_details['description'],
+                                         twitter_details['followers_count'], twitter_details['friends_count'],
+                                         twitter_details['favourites_count'], twitter_details['verified'],
+                                         twitter_details['statuses_count'], current_user_id
+                                         )
+                                        )
+                            conn.commit()
 
-                        cur.execute(config.INSERT_CITY_COUNTRY,
-                                    (trips[trip_id]['name'], country_id)
-                                    )
-                        conn.commit()
+                trips_query = config.SELECT_TRIPS.format(current_user_id)
+                cur.execute(trips_query)
+                existing_trips = cur.fetchall()
+                existing_trips_ext_ids = [t['external_id'] for t in existing_trips]
+                for trip_id in trips:
+                    if trip_id not in existing_trips_ext_ids:
 
-                        cc_id = cur.lastrowid
-                    else:
-                        cc_id = cc_id[0]['id']
+                        query = config.SELECT_CITY_COUNTRY.format(trips[trip_id]['name'], trips[trip_id]['country'])
+                        cur.execute(query)
+                        cc_id = cur.fetchall()
 
-                    trips_list.append((trip_id, current_user_id, trips[trip_id]['trip_start'],
-                                       trips[trip_id]['trip_length'], trips[trip_id]['trip_end'], cc_id))
+                        if not cc_id:
 
-            cur.executemany(config.INSERT_TRIPS_INFO, trips_list)
-            conn.commit()
+                            query2 = config.SELECT_COUNTRY
+                            cur.execute(query2, trips[trip_id]['country'])
+                            country_id = cur.fetchall()
+
+                            if not country_id:
+                                cur.execute(config.INSERT_COUNTRY, (trips[trip_id]['country']))
+                                conn.commit()
+                                country_id = cur.lastrowid
+                            else:
+                                country_id = country_id[0]['id']
+
+                            cur.execute(config.INSERT_CITY_COUNTRY,
+                                        (trips[trip_id]['name'], country_id)
+                                        )
+                            conn.commit()
+
+                            cc_id = cur.lastrowid
+                        else:
+                            cc_id = cc_id[0]['id']
+
+                        trips_list.append((trip_id, current_user_id, trips[trip_id]['trip_start'],
+                                           trips[trip_id]['trip_length'], trips[trip_id]['trip_end'], cc_id))
+
+                cur.executemany(config.INSERT_TRIPS_INFO, trips_list)
+                conn.commit()
+        except pymysql.err.InternalError as error:
+            print('error in sql. user: ', username)
+            print('dict: ', user_info)
