f586efe6b1d2c4be5d26a2c431857ddf0d513672
Rozanna Royter <rozzy.royter@gmail.com>
2020-07-31 17:18:57 +0300

    added try except for too many redirects

--- get_users.py
+++ get_users.py
@@ -27,13 +27,19 @@ def scrap_city(domain):
             (visited the city, are there now, will be there)
             https://nomadlist.com/people/berlin
     """
-    r = requests.get(domain)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    people_there = soup.find(config.ATTRIBUTES_DICT["DIV_TAG"], class_=config.ATTRIBUTES_DICT["PEOPLE"])
     people = []
-    for a in people_there.find_all(config.ATTRIBUTES_DICT["A_TAG"], href=True):
-        people.append(a[config.ATTRIBUTES_DICT["HREF"]][config.USER_LINK_CHAR_START:])
+    try:
+        r = requests.get(domain)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        people_there = soup.find(config.ATTRIBUTES_DICT["DIV_TAG"], class_=config.ATTRIBUTES_DICT["PEOPLE"])
+        for a in people_there.find_all(config.ATTRIBUTES_DICT["A_TAG"], href=True):
+            people.append(a[config.ATTRIBUTES_DICT["HREF"]][config.USER_LINK_CHAR_START:])
+    except requests.TooManyRedirects as error:
+        logger.error(error)
+        logger.error(f'For url: {domain}')
     return people
+
+
 
 
 def get_all_users(list_of_cities):
@@ -48,12 +54,15 @@ def get_all_users(list_of_cities):
         users_names = []
     for city in list_of_cities:
         domain = get_domain(city)
+        new_user_names = []
         try:
-            users_names.extend(scrap_city(domain))
+            new_user_names = scrap_city(domain)
+            users_names.extend(new_user_names)
         except FileNotFoundError:
             logger.warning(config.MSG_DICT["CITY_PAGE_NOT_FOUND"].format(city))
         users_names = list(set(users_names))
-        logger.info(config.MSG_DICT["ADDING_USERS"].format(city))
+        if len(new_user_names) > 0:
+            logger.info(config.MSG_DICT["ADDING_USERS"].format(city))
     return users_names
 
 
